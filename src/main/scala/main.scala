e3fdb81f3354d400fa7b0c381217469bfdd51fb2
Jon Pretty <jon.pretty@propensive.com>
2023-09-11 13:36:03 +0000

    Copy Java code from Baeldung `FileChannel` example

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,4 +6,25 @@ import scala.util.chaining.*
 
 @main
 def run(): Unit =
-  println("Hello World")
+  println(read())
+
+def read(): String = ""
+/*
+    try (RandomAccessFile reader = new RandomAccessFile("src/test/resources/test_read.in", "r");
+        FileChannel channel = reader.getChannel();
+        ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+
+        int bufferSize = 1024;
+        if (bufferSize > channel.size()) {
+           bufferSize = (int) channel.size();
+        }
+        ByteBuffer buff = ByteBuffer.allocate(bufferSize);
+
+        while (channel.read(buff) > 0) {
+            out.write(buff.array(), 0, buff.position());
+            buff.clear();
+        }
+
+     return new String(out.toByteArray(), StandardCharsets.UTF_8);
+    }
+*/
