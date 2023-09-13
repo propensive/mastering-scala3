7ff5b0f3a1c278a09d447045457ea4e928bbb6f7
Jon Pretty <jon.pretty@propensive.com>
2023-09-13 18:05:21 +0000

    Convert Java example to Scala

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,26 +5,19 @@ import java.io.*, java.nio.*, file.*, channels.*, charset.*
 import scala.util.chaining.*
 
 @main
-def run(): Unit =
-  println(read())
+def run(filename: String): Unit =
+  read(filename).foreach(println)
 
-def read(): String = ""
-/*
-    try (RandomAccessFile reader = new RandomAccessFile("src/test/resources/test_read.in", "r");
-        FileChannel channel = reader.getChannel();
-        ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+def read(filename: String): List[String] =
+  val reader: RandomAccessFile = RandomAccessFile(filename, "r")
+  val channel: FileChannel = reader.getChannel()
+  val buffer: ByteBuffer = ByteBuffer.allocate(channel.size().toInt.min(10))
 
-        int bufferSize = 1024;
-        if (bufferSize > channel.size()) {
-           bufferSize = (int) channel.size();
-        }
-        ByteBuffer buff = ByteBuffer.allocate(bufferSize);
+  def recur(): List[String] =
+    if channel.read(buffer) > 0
+    then String(buffer.array(), 0, buffer.position).tap { _ => buffer.clear() } :: recur()
+    else List()
 
-        while (channel.read(buff) > 0) {
-            out.write(buff.array(), 0, buff.position());
-            buff.clear();
-        }
-
-     return new String(out.toByteArray(), StandardCharsets.UTF_8);
-    }
-*/
+  try recur() finally
+    reader.close()
+    channel.close()
